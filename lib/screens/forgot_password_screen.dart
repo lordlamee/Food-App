6807aa09978613018c7c4e9129d70f36dd2b8d31
lordlamee <gabrielolamideakorede@gmail.com>
2020-07-05 20:06:01 +0100

    forgot password screen added

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -8,59 +8,61 @@ class ForgotPassword extends StatelessWidget {
   Widget build(BuildContext context) {
     screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        constraints: BoxConstraints.tight(screenSize),
-        padding: EdgeInsets.fromLTRB(20, 60, 20, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('assets/pizza.jpg'),
-            ),
-            Text(
-              'Daskar\'s',
-              style: textStyle1.copyWith(
-                  fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Cafe',
-              style: textStyle1.copyWith(fontSize: 18),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text('Forgot Password', style: textStyle3,),
-            SizedBox(
-              height: 10,
-            ),
-            Text('Please enter a new password and \n           confirm the password',style: textStyle2.copyWith(
-              color: Colors.black,
-              fontWeight: FontWeight.normal,
-            ),),
-            SizedBox(
-              height: 35,
-            ),
-            LoginTextField(
-              prefixIcon: Icon(Icons.lock_outline),
-              hintText: 'Type New Password',
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            LoginTextField(
-              prefixIcon: Icon(Icons.lock_outline),
-              hintText: 'Confirm New Password',
-            ),
-            Spacer(),
-            LongButton(
-              label: 'Submit',
-              onPressed: (){},
-            ),
-            Spacer(
-              flex: 2,
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints.tight(screenSize),
+          padding: EdgeInsets.fromLTRB(20, 60, 20, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('assets/pizza.jpg'),
+              ),
+              Text(
+                'Daskar\'s',
+                style: textStyle1.copyWith(
+                    fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Cafe',
+                style: textStyle1.copyWith(fontSize: 18),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text('Forgot Password', style: textStyle3,),
+              SizedBox(
+                height: 10,
+              ),
+              Text('Please enter a new password and \n           confirm the password',style: textStyle2.copyWith(
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+              ),),
+              SizedBox(
+                height: 35,
+              ),
+              LoginTextField(
+                prefixIcon: Icon(Icons.lock_outline),
+                hintText: 'Type New Password',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              LoginTextField(
+                prefixIcon: Icon(Icons.lock_outline),
+                hintText: 'Confirm New Password',
+              ),
+              Spacer(),
+              LongButton(
+                label: 'Submit',
+                onPressed: (){},
+              ),
+              Spacer(
+                flex: 2,
+              )
+            ],
+          ),
         ),
       ),
     );
